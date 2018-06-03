9bf9a8d6d58cbce7b6da85751691ba492c54bc50
aaron <1d20m6@gmail.com>
2018-06-03 02:11:13 -0400

    Changed how it handles analogs, probably...

--- src/captain.py
+++ src/captain.py
@@ -18,20 +18,23 @@ class JoyCaptain:
     def __init__(self):
         self.ready = False
         self.map_ = {}
-        self.analogs = [] #a list of all buttons that ahve been identified as analog
-
-
-        #begin NOT REAL CODE
         #i want like a fucking fuck what do i
-        #im sorry 
+        #im sorry
         joys = [Joystick(31,33,35,37,40),Joystick(7,11,13,15,12)]
         self.unset = joys[0].FAL()+joys[1].FAL()
-        
+
     def stupid(self, shit): #xd
-        shit = int(shit)#fuck 
+    '''
+    so bascically this converts button status (1 for pressed 0 for not pressed)
+    into gpio signals that the atari can see
+    as it turns out we want to swap them
+    i could just have them be converted to booleans and then do !whatever
+    hey aaron do that if the shit is laggy
+    '''
+        shit = int(shit)#fuck
         if shit == 0:
             return 1
-        if shit == 1 or shit == -1:
+        if shit == 1:
             return 0
 
     def handle_input(self, line):
@@ -45,14 +48,14 @@ urns a dict with a few key bits
 p                "button_id" is an ugly ass string
                 "button_status" is the number that indicates wahts gonig on
           """
-            bits = line.strip().split(',')
+            bits = line.strip().split(',')#bits meaning parts of the line, not actual bits
             if "type 00" in bits or "type 04" in bits:
                 return {"id":None,"status":None}
             else:#YEAH IT FUCKING LIKE doesnt needd to exist but its for code readability in this wasteland
-                val =  bits[-1].split(" ")[-1]    
+                val =  bits[-1].split(" ")[-1]
                 nomen = bits[0]+bits[2]
-#                type_ = bits[3] #UNCOMMENT THIS LINE WHEN U DO THE ANALOG OPTIMIZATION
-                return {"id":nomen,"status":int(val)}
+                type_ = int(bits[3].strip()[-2:]) #UNCOMMENT THIS LINE WHEN U DO THE ANALOG OPTIMIZATION #thank you past me (for the first and only time)
+                return {"id":nomen,"status":int(val),"type":type_}
 
         def filter_(input_):
             """
@@ -60,31 +63,24 @@ p                "button_id" is an ugly ass string
             POSTCONDITIONS: doesnt fuck with digital inputs
             makes us ignore analog inputs below the threshold (todo add a threshhold)
 
-
-
-
-
-
-
-
-
-
-
-
-
             so if we are ignoring this input return None NO ACTUALLY
 WHAT WE DO IS WE JUST FUCKING PRETEND THOSE ARE ALL ZERO FUCKING DUHH
 if we are keeping this input and its analog we convert it to "simple analog" where it's 1, 0, or -1 where 1 is "up OR left", 0 is in dead zone, -1 is "down or right"
             some digital dpads use this already so it makes sense
-            
+
 #todo(aaron) unfuck this formatting
             """
             if input_["status"]>300:
                 print(input_["status"],"FUCKING SORRY")
                 return "FUCK" #uncodumented bullshit sorry
+                #ok some documentation about when we fuck inputs:
+                #for some reason one of my controllers gives us an input status
+                #of like fifty thousand and then the program crashes
+                #or worse, it keeps running nd thinks we should bind that to a button
+
             if input_["id"] not in self.analogs and input_["status"]>1:
                 self.analogs.append(input_["id"])
-            if input in self.analogs:
+            if input["type"] == 3: #3 seems to mean analog
                 print("sorry no analogs today ahahah")
                 return None
                 dist = abs(input_["status"]-127) #MAGIC NUMBER i assume 127 is the neutral position for every analog input ever xd
@@ -99,18 +95,18 @@ if we are keeping this input and its analog we convert it to "simple analog" whe
                 input_["id"] = input_["id"]+"opposite"
                 input_["status"] = 1
             return input_
-                
+
         line = split(line)
         line = filter_(line)
         if line == "FUCK":
             print("FUCKED AN INPUT")
-            return 1 #fuck this crappy snes knockoff
+            return 1 #fuck this crappy snes knockoff, returning 1 to indicate a different exit statuss
         print("parsing this line",line)
         if self.ready:
             if line["id"] in self.map_.keys():
                 self.map_[line['id']](int(self.stupid(line["status"])))
         if not self.ready:
-            #we want to split the line into 3 parts and also filter 
+            #we want to split the line into 3 parts and also filter
             if line["id"] not in self.map_.keys():
                 print("bound one",len(self.unset))
                 self.map_[line["id"]] = self.unset[0]
@@ -122,7 +118,7 @@ if we are keeping this input and its analog we convert it to "simple analog" whe
 
 
 
-            
+
 def main():
     f = open("evs")
     devices = ["/dev/input/"+line for line in f.read().split("\n")][:-1]
